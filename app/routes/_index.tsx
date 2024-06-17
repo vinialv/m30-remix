1f3171236f63670b690bf28b4de4803dd1f3a85c
Vinicius Oliveira Alves <vini.alv15@hotmail.com>
2024-06-17 12:41:20 -0300

    Adicionado tooltip ao botões

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,21 @@
-import { ClientOnly } from "remix-utils/client-only";
-
-import { Suspense, useEffect, useLayoutEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { StyleSheetManager } from "styled-components";
-import { WhatsappLogo, InstagramLogo, EnvelopeSimple } from "phosphor-react";
+import {
+  WhatsappLogo,
+  InstagramLogo,
+  EnvelopeSimple,
+  CaretDown,
+} from "phosphor-react";
 
 import * as S from "@/styles";
 import M30SVG from "@/components/M30SVG";
 import { LogoAnimation } from "@/components/LogoAnimation";
+import {
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Tooltip } from "@radix-ui/react-tooltip";
 
 export default function Index() {
   const [splashScreen, setSplashScreen] = useState(true);
@@ -51,27 +60,52 @@ export default function Index() {
                 Entre em contato conosco!
               </p>
               <nav className="Contatos">
-                <a
-                  href="https://wa.me/5516982149970"
-                  target="_blank"
-                  aria-label="Link para WhatsApp"
-                >
-                  <WhatsappLogo size={32} weight="thin" />
-                </a>
-                <a
-                  href="https://www.instagram.com/m30.arq/"
-                  target="_blank"
-                  aria-label="Link para Instagram"
-                >
-                  <InstagramLogo size={32} weight="thin" />
-                </a>
-                <a
-                  href="mailto:muriloalves@m30.arq.br"
-                  target="_blank"
-                  aria-label="Link para e-mail"
-                >
-                  <EnvelopeSimple size={32} weight="thin" />
-                </a>
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <a
+                        href="https://wa.me/5516982149970"
+                        target="_blank"
+                        aria-label="Link para WhatsApp"
+                      >
+                        <WhatsappLogo size={32} weight="thin" />
+                      </a>
+                    </TooltipTrigger>
+                    <TooltipContent className="text-sm">
+                      WhatsApp
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <a
+                        href="https://www.instagram.com/m30.arq/"
+                        target="_blank"
+                        aria-label="Link para Instagram"
+                      >
+                        <InstagramLogo size={32} weight="thin" />
+                      </a>
+                    </TooltipTrigger>
+                    <TooltipContent className="text-sm">
+                      Instagram
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <a
+                        href="mailto:muriloalves@m30.arq.br"
+                        target="_blank"
+                        aria-label="Link para e-mail"
+                      >
+                        <EnvelopeSimple size={32} weight="thin" />
+                      </a>
+                    </TooltipTrigger>
+                    <TooltipContent className="text-sm">E-mail</TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
               </nav>
             </footer>
           </S.Home>
